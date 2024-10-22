a9744d43da630fa855d6803dc91064b8ee31b09e
Agustin Salguero <59067325+SalgueroAgus@users.noreply.github.com>
2024-10-22 10:46:34 -0300

    100% mobile compatibility

--- src/app/components/Gallery.tsx
+++ src/app/components/Gallery.tsx
@@ -23,13 +23,13 @@ const Gallery: React.FC = () => {
     <div id="our-services" className="overflow-hidden bg-background">
       <section className="min-h-screen w-full flex items-center py-10">
         <div className="container mx-auto px-4">
-          <h2 className="text-4xl font-bold text-center text-text font-serif mb-8">{t('Our Experience')}</h2>
-          <div className="flex flex-col md:flex-row h-[calc(100vh-180px)]">
-            <div className="w-full md:w-3/5 mb-8 md:mb-0 md:pr-4 h-full">
+          <h2 className="text-3xl md:text-4xl font-bold text-center text-text font-serif mb-6 md:mb-8">{t('Our Experience')}</h2>
+          <div className="flex flex-col md:flex-row md:h-[calc(100vh-180px)]">
+            <div className="w-full md:w-3/5 mb-6 md:mb-0 md:pr-4 h-[50vh] md:h-full">
               <Expandable list={images} autoPlay={true} className="w-full h-full" />
             </div>
             <div className="w-full md:w-2/5 md:pl-4 h-full flex flex-col">
-              <div className="flex flex-col space-y-8 h-full justify-between">
+              <div className="flex flex-col space-y-4 md:space-y-8 h-full justify-between">
                 <ServiceCard
                   icon="🕊️"
                   title={t('Pigeon Hunting')}
