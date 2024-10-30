832e322c748db800565e9d49943178a0a6be8d10
ITurres <emanueliturres.co@gmail.com>
2024-10-30 15:07:40 -0300

    refactor('Header.tsx'): Remove show/hide on scroll effect on 'Header' component

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState, useEffect, useCallback } from "react";
+import React, { useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
 import { useTranslation } from "react-i18next";
@@ -10,33 +10,6 @@ import logo from "@/app/assets/images/logo.webp";
 const Header: React.FC = () => {
   const { t } = useTranslation();
   const [isMenuOpen, setIsMenuOpen] = useState(false);
-  const [isHidden, setIsHidden] = useState(false);
-  const [lastScrollTop, setLastScrollTop] = useState(0);
-
-  const handleScroll = useCallback(() => {
-    const currentScroll = window.pageYOffset || document.documentElement.scrollTop;
-
-    if (currentScroll === 0) {
-      setIsHidden(false);
-      return;
-    }
-
-    if (currentScroll < lastScrollTop) {
-      setIsHidden(false);
-    } else {
-      setIsHidden(true);
-    }
-
-    setLastScrollTop(currentScroll <= 0 ? 0 : currentScroll);
-  }, [lastScrollTop]);
-
-  useEffect(() => {
-    window.addEventListener("scroll", handleScroll);
-
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, [lastScrollTop, handleScroll]);
 
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
@@ -45,15 +18,6 @@ const Header: React.FC = () => {
   return (
     <header
       className={`fixed top-0 left-0 w-full z-50 transition-all duration-300 bg-black opacity-80`}
-      style={{
-        transition: "transform 0.3s ease",
-        transform: isHidden ? "translateY(-100%)" : "translateY(0%)",
-        position: "fixed",
-        top: 0,
-        left: 0,
-        right: 0,
-        zIndex: 1000,
-      }}
     >
       <div className="container mx-auto px-4 py-4 flex justify-between items-center">
         <Link href="#home" scroll className="flex items-center">
